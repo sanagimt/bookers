80b8b415a0e94de08f324feacd712fe951907237
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-08 15:26:29 +0000

    create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,28 @@
 class BooksController < ApplicationController
+  
   def index
+    @book = Book.new
+  end
+
+  def create
+    #データを受け取り新規登録
+    book = Book.new(book_params)
+    #データベースに保存
+    book.save
+    #show画面にリダイレクト
+    redirect_to 'books/:id'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #ルートパス
   root to: 'homes#top'
   get '/books' => 'books#index'
-  get 'books/show'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
   get 'books/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
