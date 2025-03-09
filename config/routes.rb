7cd808938b992b71f55825d2018999d01be70bb5
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-09 05:56:18 +0000

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   #ルートパス
   root to: 'homes#top'
-  get '/books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/top' => 'homes#top'
+  delete 'lists/:id' => 'lists#destroy'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
