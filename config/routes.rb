f31b8683731883332620b720b5b9fda3a44d1a3b
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-09 00:50:16 +0000

    editページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/;id' => 'books#update'
+  patch 'books/:id' => 'books#update'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/top' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
