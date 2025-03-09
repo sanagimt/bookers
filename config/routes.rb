2311135081820a86c5ba720c48394551ecec9c93
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-09 09:45:32 +0000

    動作確認完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   #ルートパス
   root to: 'homes#top'
   get '/top' => 'homes#top'
-  delete 'lists/:id' => 'lists#destroy'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
