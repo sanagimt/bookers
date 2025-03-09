b6326823767fc891efca1d43e5247629b95dbd6a
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-09 02:46:41 +0000

    フラッシュメッセージ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
     #データベースに保存
     if @book.save 
       #show画面にリダイレクト（仮にindex）
+      flash[:notice] = 'Book was successfully created.'
       redirect_to '/books'
     else 
       @books = Book.all.order(id: :asc)
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = 'Book was successfully created.'
       redirect_to book_path(@book.id) 
     else
       render :edit
